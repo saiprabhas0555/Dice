b7960e0690b813f92af01b972c35209dda8cb4e3
Sai Prabhas Gaddam <127107012+saiprabhas0555@users.noreply.github.com>
2024-06-07 20:02:19 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomNumber2=Math.floor(Math.random()*6)+1;
 
-var image1="images/dice"+randomNumber1+".png";
-var image2="images/dice"+randomNumber2+".png";
+var image1="dice"+randomNumber1+".png";
+var image2="dice"+randomNumber2+".png";
 
 document.querySelector(".dice .img1").setAttribute("src",image1);
 document.querySelector(".dice .img2").setAttribute("src",image2);
